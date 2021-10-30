a58719b282fe0afa3c96e6cd67d93971547d7d43
Palla Gopi Krishna <88620170+190031237@users.noreply.github.com>
2021-10-30 11:38:41 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world----- pavan is your Trainer ! This is for CICD demo ,Need Bye string to pass the test so Bye";
+    return "Hello world----- Palla Gopi Krishna is your Trainer ! This is for cdd kab-4 demo ,Need Bye string to pass the test so Bye";
   }
 }
